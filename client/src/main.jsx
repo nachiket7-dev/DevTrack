59e9cb07eba80e22c275be18faf4387dd8c95532
Nachiket Amlekar <nachiketamlekar@Nachikets-MacBook-Pro.local>
2026-01-12 22:21:10 +0530

    fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,6 +9,7 @@ import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+console.log("DEBUG: Current Clerk Key:", PUBLISHABLE_KEY);
 
 if (!PUBLISHABLE_KEY) {
     throw new Error('Missing Publishable Key')
